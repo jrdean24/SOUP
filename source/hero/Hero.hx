a34ea7005e4db72f42fd54c841a1291719a1a078
Jessica Dean <jrd2017@live.missouristate.edu>
2022-05-02 17:01:45 -0500

    Added movement to hero

--- source/hero/Hero.hx
+++ source/hero/Hero.hx
@@ -1,5 +1,6 @@
 package hero;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 
@@ -11,5 +12,40 @@ class Hero extends FlxSprite
 
 		// load graphic later on
 		makeGraphic(40, 80, FlxColor.WHITE, false, "Hero");
+	}
+
+	override public function update(elapsed:Float)
+	{
+		if (FlxG.keys.pressed.LEFT)
+		{
+			velocity.x = -100;
+			velocity.y = 0;
+		}
+
+		if (FlxG.keys.pressed.RIGHT)
+		{
+			velocity.x = 100;
+			velocity.y = 0;
+		}
+
+		if (FlxG.keys.pressed.UP)
+		{
+			velocity.y = -100;
+			velocity.x = 0;
+		}
+
+		if (FlxG.keys.pressed.DOWN)
+		{
+			velocity.y = 100;
+			velocity.x = 0;
+		}
+
+		if (FlxG.keys.pressed.UP == false && FlxG.keys.pressed.DOWN == false && FlxG.keys.pressed.LEFT == false && FlxG.keys.pressed.RIGHT == false)
+		{
+			velocity.y = 0;
+			velocity.x = 0;
+		}
+
+		super.update(elapsed);
 	}
 }
