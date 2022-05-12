f68485dc33c549821a1a0b4da4f70391f324da18
Jessica Dean <jrd2017@live.missouristate.edu>
2022-05-12 00:05:30 -0500

    add potatoes in random places and add bushes

--- source/Scene3.hx
+++ source/Scene3.hx
@@ -9,6 +9,7 @@ import flixel.addons.ui.FlxButtonPlus;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
+import item.Bush;
 import item.Ingredient;
 import item.Wall;
 
@@ -19,6 +20,7 @@ class Scene3 extends FlxState
 
 	var hero:character.Hero;
 	var potatoes:FlxTypedGroup<item.Ingredient>;
+	var bushes:FlxTypedGroup<item.Bush>;
 	var potatoCount:Int = 0;
 	var walls:FlxTypedGroup<item.Wall>;
 	var returntoMap:FlxButtonPlus;
@@ -34,6 +36,7 @@ class Scene3 extends FlxState
 
 	// name to put into load graphics
 	var potatoIcon:String = "assets/images/Potato.png";
+	var bushPicture:String = "assets/images/bush.png";
 
 	public function new(incomingInventory:Inventory)
 	{
@@ -52,14 +55,37 @@ class Scene3 extends FlxState
 
 		add(new FlxSprite(0, 0, "assets/images/potatoLocal.png"));
 
-		// TODO: Put Potatoes in correct places
-		// TODO: Replace pototo.png with one without a background
-		// TODO: Add Bushes
-
 		potatoes = new FlxTypedGroup<item.Ingredient>();
-		potatoes.add(new Ingredient(350, 450, potatoIcon));
-		potatoes.add(new Ingredient(450, 450, potatoIcon));
+		potatoes.add(new Ingredient(613, 719, potatoIcon));
+		potatoes.add(new Ingredient(216, 664, potatoIcon));
+		potatoes.add(new Ingredient(462, 765, potatoIcon));
+		potatoes.add(new Ingredient(349, 423, potatoIcon));
+		potatoes.add(new Ingredient(319, 550, potatoIcon));
+		potatoes.add(new Ingredient(302, 269, potatoIcon));
+		potatoes.add(new Ingredient(885, 371, potatoIcon));
+		potatoes.add(new Ingredient(398, 345, potatoIcon));
+		potatoes.add(new Ingredient(895, 695, potatoIcon));
+		potatoes.add(new Ingredient(1113, 637, potatoIcon));
+		potatoes.add(new Ingredient(340, 581, potatoIcon));
+		potatoes.add(new Ingredient(855, 757, potatoIcon));
+		potatoes.add(new Ingredient(280, 255, potatoIcon));
+		potatoes.add(new Ingredient(448, 328, potatoIcon));
+		potatoes.add(new Ingredient(666, 244, potatoIcon));
+		potatoes.add(new Ingredient(323, 714, potatoIcon));
+		potatoes.add(new Ingredient(313, 301, potatoIcon));
+		potatoes.add(new Ingredient(1041, 340, potatoIcon));
+		potatoes.add(new Ingredient(622, 568, potatoIcon));
+		potatoes.add(new Ingredient(998, 558, potatoIcon));
 		add(potatoes);
+
+		bushes = new FlxTypedGroup<item.Bush>();
+		bushes.add(new Bush(200, 250, bushPicture));
+		bushes.add(new Bush(900, 600, bushPicture));
+		bushes.add(new Bush(250, 400, bushPicture));
+		bushes.add(new Bush(325, 600, bushPicture));
+		bushes.add(new Bush(625, 550, bushPicture));
+		bushes.add(new Bush(675, 350, bushPicture));
+		add(bushes);
 
 		hero = new Hero(50, 50);
 		add(hero);
@@ -96,9 +122,9 @@ class Scene3 extends FlxState
 	public function AddPotato(obj1:flixel.FlxBasic, obj2:flixel.FlxBasic)
 	{
 		obj1.kill();
-		potatoCount += 1;
-		potatoNum.text = "Potatoes: " + potatoCount;
-		add(potatoNum);
+		potatoCount++;
+		inventory.addPotatoes(1);
+		potatoNum.text = "Potatoes: " + inventory.potatoes;
 	}
 
 	override public function update(elapsed:Float)
